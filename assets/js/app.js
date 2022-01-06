6269ffede95f53f71a87f90b0ba08407629407fb
Richard Bernisca <taropogi_123@yahoo.com>
2022-01-06 23:20:57 +0800

    add links

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,9 +3,63 @@ const app = Vue.createApp({
     return {
       selectGenderText: "Select Gender",
       selectAgeText: "Select Age",
+      age1: "18 - 30 years old",
+      age2: "30 - 40 years old",
+      age3: "40 - 50 years old",
+      age4: "50+ years old",
     };
   },
+  computed: {
+    formLink() {
+      //Men
+      if (this.selectGenderText === "Men" && this.selectAgeText === this.age1) {
+        return "https://www.google.com/";
+      }
+      if (this.selectGenderText === "Men" && this.selectAgeText === this.age2) {
+        return "https://www.facebook.com/";
+      }
+      if (this.selectGenderText === "Men" && this.selectAgeText === this.age3) {
+        return "https://www.youtube.com/";
+      }
+      if (this.selectGenderText === "Men" && this.selectAgeText === this.age4) {
+        return "https://twitter.com/";
+      }
+
+      //Women
+      if (
+        this.selectGenderText === "Women" &&
+        this.selectAgeText === this.age1
+      ) {
+        return "https://www.instagram.com/";
+      }
+      if (
+        this.selectGenderText === "Women" &&
+        this.selectAgeText === this.age2
+      ) {
+        return "https://upwork.com/";
+      }
+      if (
+        this.selectGenderText === "Women" &&
+        this.selectAgeText === this.age3
+      ) {
+        return "https://stackoverflow.com/";
+      }
+      if (
+        this.selectGenderText === "Women" &&
+        this.selectAgeText === this.age4
+      ) {
+        return "https://www.mysql.com/";
+      }
+
+      return "";
+    },
+  },
   methods: {
+    submitForm() {
+      if (this.formLink !== "") {
+        window.location.href = this.formLink;
+      }
+    },
     setSelectGenderText(txt) {
       this.selectGenderText = txt;
     },
